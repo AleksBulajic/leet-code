1cc9d043bb90531b68e2aa361d073d28f1c76716
AleksBulajic <aleks.bulajic25@gmail.com>
2023-10-30 16:00:28 -0400

    added some comments

--- leetcode-codewars.js
+++ leetcode-codewars.js
@@ -10,20 +10,28 @@ function booleanToWord(bool){
 console.log(booleanToWord(true))
 console.log(booleanToWord(false))
 
-/*  2.Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
-
+/*  
+*2.Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+*
 For example,
 
-[true,  true,  true,  false,
-  true,  true,  true,  true ,
-  true,  false, true,  false,
-  true,  false, false, true ,
-  true,  true,  true,  true ,
-  false, false, true,  true]
-The correct answer would be 17.
+* [true,  true,  true,  false,
+ * true,  true,  true,  true ,
+ * true,  false, true,  false,
+   *true,  false, false, true ,
+ * true,  true,  true,  true ,
+ * false, false, true,  true]
+*The correct answer would be 17.
 
-Hint: Don't forget to check for bad values like null/undefined */
+* Hint: Don't forget to check for bad values like null/undefined */
 
 function countSheeps(sheep){
-    
+
 }
+
+/*  
+* Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+*
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+*
+[10, 343445353, 3453445, 3453545353453] should return 3453455. */
