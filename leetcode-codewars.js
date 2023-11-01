d229381a41e474937ed72d1f951b4d8c8a222aab
AleksBulajic <aleks.bulajic25@gmail.com>
2023-11-01 11:18:19 -0400

    problem 5 solved

--- leetcode-codewars.js
+++ leetcode-codewars.js
@@ -91,3 +91,24 @@ function findNeedle(array){
 console.log(findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"]));
 console.log(findNeedle(["hay", "needle", "hay", "hay"]));
 console.log(findNeedle(["hay", "junk", "hay", "hay", "moreJunk", "randomJunk"]));
+
+
+/*
+ * Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+* Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+*/
+
+
+function loveFunc( flower1, flower2){
+if((flower1 % 2 === 0 && flower2 % 2 !== 0 ) || (flower1 % 2 !== 0 && flower2 % 2 === 0 )){
+  return true
+}else {
+  return false
+}
+}
+
+console.log(loveFunc(1, 4)); // This should return true
+console.log(loveFunc(2, 2)); // This should return false
+console.log(loveFunc(3, 6)); // This should return true
+console.log(loveFunc(4, 8)); // This should return false
