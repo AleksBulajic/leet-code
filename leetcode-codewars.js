b87afb8eb21cba200ab58792653921025cb839bf
AleksBulajic <aleks.bulajic25@gmail.com>
2023-12-07 00:58:36 -0500

    having a hard time wiht problem 20

--- leetcode-codewars.js
+++ leetcode-codewars.js
@@ -497,9 +497,10 @@ function romanToInt(str){
 
 let result = 0;
 
-
-
-
+for(let i = 0; i < str.length;i++){
+let curentSymbol = str[i].toLowerCase()
+let currntSymboleValue = romanValues[curentSymbol]
+}
 
 }
 
