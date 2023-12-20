e742c6cbd7269a5e0154456b145bd5416b550e1f
AleksBulajic <aleks.bulajic25@gmail.com>
2023-12-20 15:48:23 -0500

    added a setTimeOut problem!

--- leetcode-codewars.js
+++ leetcode-codewars.js
@@ -565,3 +565,15 @@ console.log(romanToInt("IV"));
 console.log(romanToInt("IX"));
 console.log(romanToInt("LVIII"));
 console.log(romanToInt("MCMXCIV"));
+
+
+// A fellow student shows you this code.  When he runs it, he expects it to
+// wait three seconds, then write "Ding!" to the console.  Instead, it writes
+// "Ding!" immediately.  Find the bug and fix it.
+
+function writeDing() {
+  console.log('Ding!');
+}
+
+// let timerId = setTimeout(writeDing(), 3000);
+let timerId = setTimeout(writeDing,3000)
